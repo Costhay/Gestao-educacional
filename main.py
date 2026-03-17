7250f9f5fbe441f73a7dcc34547bd7e6a8e2bcb4
THAYNA ASSIS DA COSTA <37125795862@info.local>
2026-03-17 19:46:58 -0300

    Edicao de formatacao

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-linhat = ("▁" * 120)
+linhat = ("༟" * 120)
 titulo1 = ("GESTÃO DE NOTAS E DESEMPENHO DE ALUNOS")
 present = ("""Esta aplicação tem como objetivo auxiliar a equipe de coordenação e docentes na gestão de notas e desempenho acadêmico.
-Com esta aplicação, os professores podem cadastrar alunos, atribuir notas e acompanhar o resultado do aluno.
-           """)
+Com esta aplicação, os professores podem cadastrar alunos, atribuir notas e acompanhar o resultado do aluno.""")
 
 print(linhat)
 print(titulo1)
 print(linhat)
 print(present)
 print(linhat)
+
+
+
 
