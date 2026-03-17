5df528a6766747cd61b6fd64282b688a41b48ebf
THAYNA ASSIS DA COSTA <37125795862@info.local>
2026-03-17 18:38:35 -0300

    Primeiro commit arquivo main

---
+++ processamento.py
@@ -0,0 +1,4 @@
+# O processamento das notas recebidas serão realizados nesse documento, para ser importado para main.py, por modularização
+
+# Função para receber as notas e nomes dos alunos, e criar uma lista com tuplas
+
